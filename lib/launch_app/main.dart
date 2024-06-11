bcee0daea218fc4e365caf881bad30070630d210
mhmmddzaky <muh.dzaky1919@gmail.com>
2024-06-11 16:30:19 +0700

    halaman register penyelenggara

---
+++ lib/launch_app/main.dart
@@ -0,0 +1,20 @@
+import 'package:final_project_quickregist/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
